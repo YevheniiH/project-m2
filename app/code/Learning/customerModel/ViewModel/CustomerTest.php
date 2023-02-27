66d0119a83ecf8f789a691a3344e1349e64e4295
Yevhenii Hanzha <evgenijganza369@gmail.com>
2023-02-27 16:57:58 +0200

    MAG-17 Create View Model
    -add login checking method from the class; -method which will receive JSON;

---
+++ app/code/Learning/customerModel/ViewModel/CustomerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Learning\customerModel\ViewModel;
+
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+use Magento\Customer\Block\Account\Customer;
+
+class CustomerTest implements ArgumentInterface
+{
+    /**
+     * @var Customer
+     */
+    private $customer;
+
+    /**
+     * @param Customer $customer
+     */
+    public function __construct(Customer $customer)
+    {
+        $this->customer = $customer;
+    }
+
+    public function getSomething()
+    {
+       return "-Test-";
+    }
+
+    /**
+     * @return bool
+     */
+    public function isUserLoggedIn()
+    {
+        return $this->customer->customerLoggedIn();
+    }
+
+    public function getArrayJson($jsonSting)
+    {
+        return json_decode($jsonSting, true);
+    }
+
+}
+
+
