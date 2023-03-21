7350a6e4631523c89c43e5bd18f95cee434b054e
Yevhenii Hanzha <evgenijganza369@gmail.com>
2023-03-21 10:13:42 +0200

    MAG-24 Create mixin for widget

---
+++ app/design/frontend/Jungle-luma/jungle_theme/Magento_Theme/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Theme/js/zoom-image': {
+                'Magento_Theme/js/mixin/zoom-image-widget-mixin': true
+            }
+        }
+    }
+};
+
